950d08e3ab0c33fc9bcb8fe5b1d40b057c0c7eff
Targan Dev <targancrypto@gmail.com>
2026-06-05 02:25:37 +0330

    Update MyERC20.sol

--- contracts/MyERC20.sol
+++ contracts/MyERC20.sol
@@ -4,13 +4,24 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title Targan Token
+ * @dev Secure ERC20 token with minting capabilities restricted to the owner.
+ */
 contract TarganToken is ERC20, Ownable {
+
     constructor() ERC20("Targan Token", "TARGAN") Ownable(msg.sender) {
+        // Minting 1,000,000 tokens to the deployer on creation
         _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
 
-    // فقط Owner می‌تواند توکن جدید mint کند
+    /**
+     * @notice Mints new tokens to a specific address.
+     * @dev Restricted to the contract owner via onlyOwner modifier.
+     * @param to The address that will receive the minted tokens.
+     * @param amount The amount of tokens to mint (will be automatically adjusted for decimals).
+     */
     function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+        _mint(to, amount * 10 ** decimals());
     }
 }
